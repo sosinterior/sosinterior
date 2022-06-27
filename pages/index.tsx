17eff0d52286404901a8b215400336fbea927781
Mert Ozyilmaz <mertozyilmazz@gmail.com>
2022-06-27 12:00:56 +0300

    home mobile header border deleted

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import Footer from '@/components/footer'
 const Home: NextPage = () => {
     return (
         <>
-            <Header isBorder={true} />
+            <Header />
             <main>
                 <HomeSlider />
                 <HomeSliderBottom />
